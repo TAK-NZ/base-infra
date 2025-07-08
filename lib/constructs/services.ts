d55865e9a1057fc5e8519619fb307d3856001b3e
Christian Elsen <chris@chriselsen.net>
2025-07-08 04:43:39 +0000

    feat: add ALB logs bucket with service account permissions
    - Add albLogsBucket with globally unique naming pattern
    - Grant ALB service account permissions for all AWS regions
    - Add PutObject and GetBucketAcl permissions for ALB access logs
    - Update test to expect 4 S3 buckets instead of 3
    - Remove duplicate permission code

--- lib/constructs/services.ts
+++ lib/constructs/services.ts
@@ -108,5 +108,35 @@ export function createS3Resources(scope: Construct, stackName: string, region: s
     objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
   });
 
+  // Grant ALB service account permission to write access logs
+  const elbServiceAccountMap: { [key: string]: string } = {
+    'us-east-1': '127311923021', 'us-east-2': '033677994240', 'us-west-1': '027434742980', 'us-west-2': '797873946194',
+    'ca-central-1': '985666609251', 'eu-central-1': '054676820928', 'eu-west-1': '156460612806', 'eu-west-2': '652711504416',
+    'eu-west-3': '009996457667', 'eu-north-1': '897822967062', 'eu-south-1': '635631232127', 'ap-east-1': '754344448648',
+    'ap-northeast-1': '582318560864', 'ap-northeast-2': '600734575887', 'ap-northeast-3': '383597477331', 'ap-southeast-1': '114774131450',
+    'ap-southeast-2': '783225319266', 'ap-southeast-3': '589379963580', 'ap-south-1': '718504428378', 'me-south-1': '076674570225',
+    'sa-east-1': '507241528517', 'af-south-1': '098369216593', 'us-gov-west-1': '048591011584', 'us-gov-east-1': '190560391635'
+  };
+  
+  const elbPrincipal = elbServiceAccountMap[region] 
+    ? new cdk.aws_iam.AccountPrincipal(elbServiceAccountMap[region])
+    : new cdk.aws_iam.ServicePrincipal('elasticloadbalancing.amazonaws.com');
+  
+  albLogsBucket.addToResourcePolicy(new PolicyStatement({
+    effect: Effect.ALLOW,
+    principals: [elbPrincipal],
+    actions: ['s3:PutObject'],
+    resources: [`${albLogsBucket.bucketArn}/*`]
+  }));
+  
+  albLogsBucket.addToResourcePolicy(new PolicyStatement({
+    effect: Effect.ALLOW,
+    principals: [elbPrincipal],
+    actions: ['s3:GetBucketAcl'],
+    resources: [albLogsBucket.bucketArn]
+  }));
+
+
+
   return { configBucket, envConfigBucket, appImagesBucket, albLogsBucket };
 }
